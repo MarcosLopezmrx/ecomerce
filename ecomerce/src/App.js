76e97257b1ac1334fcae4abb1ca3ff7ce9ea5585
MarcosLopezmrx <lopezjmarcostec@hotmail.com>
2022-02-21 19:46:01 -0300

    navbarsi

--- ecomerce/src/App.js
+++ ecomerce/src/App.js
@@ -2,20 +2,22 @@
 import './App.css'
 
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Navbar from './components/Navbar1';
+import Navbar from './components/Navbar';
 import Inicio from './components/Paginas/Inicio';
 import Contacto from './components/Paginas/Contacto';
 function App() {
-
+  return(
 <div className="App">
+
   <h1>Navbar</h1>
 
-  <router>
-    <navbar/>
-  </router>
+  <Router>
+    <Navbar/>
+  </Router>
 
 
 
 </div>
+  );
 }
 export default App;
