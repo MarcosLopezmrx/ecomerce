915c6f5110dbe35d97035f3d7a72ea17e34d2940
MarcosLopezmrx <lopezjmarcostec@hotmail.com>
2022-02-20 21:01:15 -0300

    navfail

--- ecomerce/src/App.js
+++ ecomerce/src/App.js
@@ -1,25 +1,23 @@
+import { useState } from 'react'
 import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import navBar from './components/navbar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [count, setCount] = useState(0)
+  const style = {backgroundColor: 'blue'}
+
+  return(
+    <div 
+    className='App'
+    style={style}
+    >
+      <navBar />
+      <input />
+      hola soy una app
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ ecomerce/src/components/navbar.jsx
@@ -0,0 +1,9 @@
+
+
+
+const navBar = () => {
+    return{
+    <h1> hola </h1>
+    }
+}
+export default navBar
