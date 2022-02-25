7abfa3e0f2ac76fd8fa1a8796aa6697683757915
MarcosLopezmrx <lopezjmarcostec@hotmail.com>
2022-02-25 11:07:08 -0300

    render

--- ecomerce/src/App.js
+++ ecomerce/src/App.js
@@ -4,12 +4,13 @@ import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import Navbar from './components/Navbar';
 import Inicio from './components/Paginas/Inicio';
 import Contacto from './components/Paginas/Contacto';
+import CartWidget from './components/CartWidget';
 function App() {
   return(
 <div className="App">
     <Navbar/>
-    <ItemListContainer/>
   <h1>Navbar</h1>
+  <CartWidget/>
 
 </div>
   );
---
+++ ecomerce/src/components/CartWidget.jsx
@@ -0,0 +1,11 @@
+import React, {Component } from 'core-js/library/fn/reflect/es7/metadata'
+
+
+render(){
+    return(
+        <img src="IMG/carrito.png" alt="" />
+    )
+}
+
+
+export default CartWidget
